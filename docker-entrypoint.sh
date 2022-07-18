61c6726a9a20fe13c5d16d6b50109176900a94d0
Jeremy MAURO <jmauro@aviatrix.com>
2022-07-18 22:05:27 +0200

    fix(tty): Check if a TTY attached to display headers
    Signed-off-by: Jeremy MAURO <jmauro@aviatrix.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,18 +21,23 @@ LIGHT_CYAN="\e[96m"
 B="\e[1m"
 U="\e[4m"
 
-# Getting information you need to display
-AWS_LIST="$(aws configure list | egrep 'profile|region'| sed -e 's/<not set>/default/g' | awk '{print $2}')"
-PROFILE="$(echo ${AWS_LIST} | cut -d' ' -f1)"
-AWS_REGION="$(echo ${AWS_LIST} | cut -d' ' -f2)"
-CLUSTER="$(kubectx -c)"
-NAMESPACE="$(kubens -c)"
+if [[ -t 1 ]]; then
+  # Display information when attached to TTY
+  # Ref: https://stackoverflow.com/questions/911168/how-can-i-detect-if-my-shell-script-is-running-through-a-pipe/30520299#30520299
 
-printf "\n----------------------------------------------------\n"
-printf "${B}>>> ${BLUE}%-19b${LIGHT_BLUE}: %-s${N}\n" "${U}AWS PROFILE${N}" "${PROFILE}"
-printf "${B}>>> ${CYAN}%-19b${LIGHT_CYAN}: %-s${N}\n" "${U}AWS REGION${N}" "${AWS_REGION}"
-printf "${B}>>> ${GREEN}%-19b${LIGHT_GREEN}: %-s${N}\n" "${U}CLUSTER${N}" "${CLUSTER}"
-printf "${B}>>> ${YELLOW}%-19b${LIGHT_YELLOW}: %-s${N}\n" "${U}NAMESPACE${N}" "${NAMESPACE}"
-printf -- "----------------------------------------------------\n"
+  # Getting information you need to display
+  AWS_LIST="$(aws configure list | egrep 'profile|region'| sed -e 's/<not set>/default/g' | awk '{print $2}')"
+  PROFILE="$(echo ${AWS_LIST} | cut -d' ' -f1)"
+  AWS_REGION="$(echo ${AWS_LIST} | cut -d' ' -f2)"
+  CLUSTER="$(kubectx -c)"
+  NAMESPACE="$(kubens -c)"
+
+  printf "\n----------------------------------------------------\n"
+  printf "${B}>>> ${BLUE}%-19b${LIGHT_BLUE}: %-s${N}\n" "${U}AWS PROFILE${N}" "${PROFILE}"
+  printf "${B}>>> ${CYAN}%-19b${LIGHT_CYAN}: %-s${N}\n" "${U}AWS REGION${N}" "${AWS_REGION}"
+  printf "${B}>>> ${GREEN}%-19b${LIGHT_GREEN}: %-s${N}\n" "${U}CLUSTER${N}" "${CLUSTER}"
+  printf "${B}>>> ${YELLOW}%-19b${LIGHT_YELLOW}: %-s${N}\n" "${U}NAMESPACE${N}" "${NAMESPACE}"
+  printf -- "----------------------------------------------------\n"
+fi
 
 exec "${@}"
